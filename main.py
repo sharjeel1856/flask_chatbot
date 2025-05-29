259416f26a33dfdb67da2fb84eb24fb54ebc9b46
sharjeel <awansharjeel1122@gmail.com>
2025-05-29 15:10:28 +0430

    Updated app for Railway deployment

--- main.py
+++ main.py
@@ -356,4 +356,7 @@ def teacher_input():
     )
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
